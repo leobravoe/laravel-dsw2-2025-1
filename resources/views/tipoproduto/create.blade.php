d70857a4adf098d2b4b2ba481164cda75bd8d16c
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-06-02 14:29:04 -0300

    inicio aula 31 e 32

--- resources/views/tipoproduto/create.blade.php
+++ resources/views/tipoproduto/create.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create de TipoProduto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </head>
@@ -13,10 +13,7 @@
 <body>
     <div class="container">
         <form method="post" action="/tipoproduto">
-            
             @CSRF
-
-
             <div class="my-3">
                 <label for="id-input-id" class="form-label">ID</label>
                 <input id="id-input-id" type="text" class="form-control" aria-describedby="id-div-help" value="#" disabled>
